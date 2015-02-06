b129a05f7346c6edb0c8fbb6867b63337d485403
Lamb11 <abc_1118@hotmail.com>
2015-02-05 20:20:38 -0600

    Ya hice la cámara

---
+++ BaseD.h
@@ -0,0 +1,27 @@
+//
+//  BaseD.h
+//  Lab03
+//
+//  Created by Alberto Cordero Arellanes on 05/02/15.
+//  Copyright (c) 2015 AlbertoCorderoArellanes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "sqlite3.h"
+
+extern int valorNuevo;
+
+@interface BaseD : NSObject
+{
+    NSString *databasePath;
+}
+
++(BaseD*)getSharedInstance;
+
+-(BOOL)createDB;
+-(BOOL)saveData:(int)puntos fecha:(NSString*)fecha;
+-(NSArray*) findByRegisterNumber:(NSString*)registerNumber;
+-(NSArray*) listado;
+-(NSMutableArray*) listar;
+
+@end
